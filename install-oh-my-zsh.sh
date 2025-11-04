1eaf49480361d8241d598dca02aea7914ee91ec5
killf <killf@foxmail.com>
2025-11-04 15:52:09 +0800

    Use sudo for installing zsh in script

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -1,4 +1,4 @@
-apt install zsh -y
+sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
